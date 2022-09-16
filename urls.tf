bd31b2dab2c9075899e31d423e257c0d50ca656b
Fouad Chmainy <fchmainy@yahoo.com>
2022-09-16 11:16:53 +0200

    Update urls.tf

--- urls.tf
+++ urls.tf
@@ -1,6 +1,6 @@
 variable "urls" {
   type                        = map(object({
-  name             	          = string
+  name                        = string
   description                 = string
   type	                      = string
   protocol	                  = string
