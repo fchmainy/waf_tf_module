57b367e8dae77476f51210cce9854320540c5122
Fouad Chmainy <fchmainy@yahoo.com>
2022-09-16 09:29:59 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable provider_version {}
+variable bigips {}
+variable name {}
+variable partition {}
+variable template_name {}
+variable application_language {}
+variable enforcement_mode {}
+variable server_technologies {}
+variable parameters {}
+variable signatures {}
+variable modifications {}
+variable tags {}
