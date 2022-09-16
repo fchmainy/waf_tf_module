91835c3117a1e158bb96e910d2cfe16e723404f3
Fouad Chmainy <fchmainy@yahoo.com>
2022-09-16 10:12:48 +0200

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,7 +1,7 @@
 resource "bigip_waf_policy" "this" {
   provider              = var.bigip
   name                  = var.name
-  partition		          = var.partition
+  partition             = var.partition
   template_name         = var.template_name
   application_language  = var.application_language
   enforcement_mode      = var.enforcement_mode
