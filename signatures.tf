71f9293c927dfcf50cb1a784e437491db9600f19
Fouad Chmainy <fchmainy@yahoo.com>
2022-09-16 11:24:59 +0200

    Create signatures.tf

---
+++ signatures.tf
@@ -0,0 +1,29 @@
+variable "signatures" {
+  type                        = map(object({
+  name                        = string
+  description                 = string
+  signature_id                = number
+  enabled 	                  = bool
+  tag                         = string
+  type                        = string
+  accuracy                    = string
+  perform_staging             = boolean
+  }))
+}
+
+
+data "bigip_waf_signatures "this" {
+  for_each	                  = var.signatures
+  name			                  = each.value["name"]
+  description                 = each.value["description"]
+  signature_id                = each.value["signature_id"]
+  enabled		                  = each.value["enabled"]
+  tag                         = each.value["tag"]
+  type                        = each.value["type"]
+  accuracy        	          = each.value["accuracy"]
+  perform_staging             = each.value["perform_staging"]
+}
+
+output "urls" {
+  value = [ for k,v in data.bigip_waf_signatures.this: v.json ]
+  }
