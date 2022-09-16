97924aaf4a12d983306f49aa74bdfbb41ff7ab41
Fouad Chmainy <fchmainy@yahoo.com>
2022-09-16 10:50:06 +0200

    Update parameters.tf

--- parameters.tf
+++ parameters.tf
@@ -1,9 +1,9 @@
 variable "parameters" {
   type                        = map(object({
-  name             	      = string
+  name             	          = string
   description                 = string
   type	                      = string
-  data_type	              = string
+  data_type	                  = string
   value_type                  = string
   is_header                   = boolean
   is_cookie                   = boolean
@@ -16,17 +16,21 @@ variable "parameters" {
 
 
 data "bigip_waf_entity_parameter" "this" {
-  for_each	              = var.local_parameters
-  name			      = each.value["name"]
-  type			      = each.value["type"]
+  for_each	                  = var.local_parameters
+  name			                  = each.value["name"]
+  type			                  = each.value["type"]
   description                 = each.value["description"]
-  type			      = each.value["type"]
-  data_type		      = each.value["data_type"]
+  type			                  = each.value["type"]
+  data_type		                = each.value["data_type"]
   value_type                  = each.value["value_type"]
   is_header                   = each.value["is_header"]
   is_cookie                   = each.value["is_cookie"]
   mandatory                   = each.value["mandatory"]
-  perform_staging 	      = each.value["perform_staging"]
+  perform_staging 	          = each.value["perform_staging"]
   sensitive_parameter         = each.value["sensitive_parameter"]
   signature_overrides_disable = each.value["signature_overrides_disable"]
 }
+
+output "parameters" {
+  value = [ for k,v in data.bigip_waf_entity_parameter.this: v.json ]
+  }
